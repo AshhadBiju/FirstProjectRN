bc89fb8826a695a5e4ea8f7898d948c2191b48d6
unknown <ashhadbiju2000@gmail.com>
2023-05-21 20:49:54 +0530

    APIcallFlatlist

--- src/Tab/Category.js
+++ src/Tab/Category.js
@@ -1,12 +1,19 @@
 /* eslint-disable prettier/prettier */
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, ActivityIndicator} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+  FlatList,
+} from 'react-native';
 
 export default class Category extends Component {
   constructor() {
     super();
     this.state = {
       loader: false,
+      DATA: [],
     };
   }
 
@@ -15,6 +22,9 @@ export default class Category extends Component {
     fetch('https://api.sampleapis.com/coffee/hot')
       .then(response => response.json())
       .then(response => {
+        if (response.length > 0) {
+          this.setState({DATA: response});
+        }
         this.setState({loader: false});
         console.log('YOUR RESPONSE is:', response);
       })
@@ -29,10 +39,25 @@ export default class Category extends Component {
   }
 
   render() {
+    const renderItem = ({item}) => (
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemText}>{item.title}</Text>
+        <Text style={styles.itemDes}>{item.description}</Text>
+      </View>
+    );
     return (
       <View style={styles.container}>
-        <ActivityIndicator size={'large'} animating={this.state.loader} />
-        <Text style={styles.categoryText}>Hi This is Category page</Text>
+        <ActivityIndicator
+          size={'large'}
+          color="blue"
+          animating={this.state.loader}
+        />
+        <Text style={styles.categoryText}>Hi This is Category List</Text>
+        <FlatList
+          style={{width: '95%', marginTop: 10}}
+          data={this.state.DATA}
+          renderItem={renderItem}
+        />
       </View>
     );
   }
@@ -43,11 +68,29 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'silver',
+    backgroundColor: '#eeeeee',
   },
   categoryText: {
     fontSize: 24,
     fontWeight: 'bold',
-    color: '#ffffff',
+    color: 'black',
+  },
+  itemContainer: {
+    width: '100%',
+    padding: 10,
+    backgroundColor: '#fff',
+    elevation: 4,
+    marginBottom: 10,
+  },
+  itemText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  itemDes: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#369',
+    marginTop: 10,
   },
 });
